16f3c1fd207048bad280fe23ea8a83de46971bd5
bprlab <bprlab@10.201.210.46>
2019-01-05 02:54:25 -0800

    java 1

--- hesaplama/src/main/java/tr/edu/medipol/hesaplama/Dortislem.java
+++ hesaplama/src/main/java/tr/edu/medipol/hesaplama/Dortislem.java
@@ -3,10 +3,12 @@ package tr.edu.medipol.hesaplama;
 public class Dortislem {
 	
 	public int toplama(int sayi1, int sayi2) {
+		System.out.println("toplama islemi yapılıyor");
 		return sayi1 + sayi2;
 	}
 	
 	public int cikarma(int SAYI1, int sayi2) {
+		System.out.println("cikarma islemi yapılıyor");
 		return SAYI1 - sayi2;
 	}
 	
